a61f6dd85ab5e14a3adbac4c96ea0428d65c32ae
Chaeyeoncho <gcy9293@naver.com>
2025-08-04 23:04:00 +0900

    몸풀기 1번 문제

---
+++ 배열/조채연/array01.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class array01 {
+
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(solution(new int[]{1, -5, 2, 4, 3})));
+//        System.out.println(Arrays.toString(solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+//        System.out.println(Arrays.toString(solution(new int[]{6, 1, 7})));
+//    }
+//
+//    private static int[] solution(int[] arr) {
+//        Arrays.sort(arr);
+//        return arr;
+//    }
+
+    public static void main(String[] args){
+        int[] arr1 = {1, -5, 2, 4, 3};
+        int[] arr2 = {2, 1, 1, 3, 2, 5, 4};
+        int[] arr3 = {6, 1, 7};
+
+        Arrays.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+        Arrays.sort(arr2);
+        System.out.println(Arrays.toString(arr2));
+        Arrays.sort(arr3);
+        System.out.println(Arrays.toString(arr3));
+
+    }
+}
