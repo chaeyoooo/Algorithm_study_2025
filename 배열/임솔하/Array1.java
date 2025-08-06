30478b81596a242614b3d2f4ad7eb53d473873e2
sohlha0203 <limsohlha@gmail.com>
2025-08-07 02:53:27 +0900

    108페이지_array문제01

---
+++ 배열/임솔하/Array1.java
@@ -0,0 +1,24 @@
+package beforestudy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+
+
+    }
+}
